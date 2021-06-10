77bef70264a4835f2399a94fa3ef65b117191432
Mummelumsen <hfhfjgjg@live.dk>
2021-06-10 13:45:28 +0200

    fix serial

--- Arduino code/FFT_speed_test/FFT_speed_test.ino
+++ Arduino code/FFT_speed_test/FFT_speed_test.ino
@@ -64,6 +64,11 @@ double vImag[samples];
 
 void setup() {
   Serial.begin(115200);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+
+  Serial.println("startup");
 }
 
 
