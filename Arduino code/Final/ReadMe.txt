c8fca3ff8e52c49fa8418e2e4429a83364af7330
Anders J. Jørgensen <40024706+s173943@users.noreply.github.com>
2021-06-23 10:42:59 +0200

    Final commit 2

---
+++ Arduino code/Final/ReadMe.txt
@@ -0,0 +1,2 @@
+IoT_host is responsible for the remote IoT functionality, as there is a conflict in the main code.
+The main code is in the current folder.
---
+++ Arduino code/Final/approxFFT.h
@@ -0,0 +1,81 @@
+/*
+
+	FFT libray
+	Copyright (C) 2010 Didier Longueville
+	Copyright (C) 2014 Enrique Condes
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+#ifndef approxFFT_h /* Prevent loading library twice */
+#define approxFFT_h
+#ifdef ARDUINO
+	#if ARDUINO >= 100
+		#include "Arduino.h"
+	#else
+		#include "WProgram.h" /* This is where the standard Arduino code lies */
+	#endif
+#else
+	#include <stdlib.h>
+	#include <stdio.h>
+	#ifdef __AVR__
+		#include <avr/io.h>
+		#include <avr/pgmspace.h>
+	#endif
+#endif
+
+
+/*Mathematial constants*/
+#define twoPi 6.28318531
+//---------------------------------lookup data------------------------------------//
+const uint8_t isin_data[128] =
+{ 0,  1,   3,   4,   5,   6,   8,   9,   10,  11,  13,  14,  15,  17,  18,  19,  20,
+  22,  23,  24,  26,  27,  28,  29,  31,  32,  33,  35,  36,  37,  39,  40,  41,  42,
+  44,  45,  46,  48,  49,  50,  52,  53,  54,  56,  57,  59,  60,  61,  63,  64,  65,
+  67,  68,  70,  71,  72,  74,  75,  77,  78,  80,  81,  82,  84,  85,  87,  88,  90,
+  91,  93,  94,  96,  97,  99,  100, 102, 104, 105, 107, 108, 110, 112, 113, 115, 117,
+  118, 120, 122, 124, 125, 127, 129, 131, 133, 134, 136, 138, 140, 142, 144, 146, 148,
+  150, 152, 155, 157, 159, 161, 164, 166, 169, 171, 174, 176, 179, 182, 185, 188, 191,
+  195, 198, 202, 206, 210, 215, 221, 227, 236
+};
+const uint16_t Pow2[14] = {1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096};
+const uint8_t RSSdata[20] = {7, 6, 6, 5, 5, 5, 4, 4, 4, 4, 3, 3, 3, 3, 3, 3, 3, 2, 2, 2};
+//---------------------------------------------------------------------------------//
+
+
+
+
+//-----------------------------FFT Function---------------------------------------//
+void Approx_FFT(int in[], int N, int *out_r);
+//--------------------------------------------------------------------------------//
+
+//---------------------------------fast sine/cosine-------------------------------//
+int fast_sine(int Amp, int th);
+
+int fast_cosine(int Amp, int th);
+//--------------------------------------------------------------------------------//
+
+
+//--------------------------------Fast RSS----------------------------------------//
+int fastRSS(int a, int b);
+//--------------------------------------------------------------------------------//
+
+//--------------------------------binning-----------------------------------------//
+//void binning_4(int N, int *dataIn, int *dataOut);
+void binning_9(int N, int *dataIn, int *dataOut);
+uint16_t newMax(uint16_t dataToCompare, int potentialReplacement);
+void binning_12_16(uint16_t N, int *dataIn, uint16_t *dataOut);
+//--------------------------------------------------------------------------------//
+#endif
